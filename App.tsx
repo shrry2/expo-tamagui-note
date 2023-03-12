b3f3e5565407aa972e9b799ef70c60fe2071dd53
Takaki Takeuchi <takaki@takeu.ch>
2023-03-12 18:27:06 +0900

    refactor: remove unused line

--- App.tsx
+++ App.tsx
@@ -10,7 +10,6 @@ export default function App() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
-
     InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
   });
 
